2d34a09886cca359e513691fa6e382076907a0e1
Bokstret <43569571+Bokstret@users.noreply.github.com>
2021-11-16 21:02:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__, static_url_path='')
+
+app.secret_key = 'BAD_SECRET_KEY'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vtzefyhigwptbe:45eec3aaa53b4719cf61196aad25f2b5ca825611542238856d9801fc557c34f1@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/d1pfe0st2g9c16'
 db = SQLAlchemy(app)
 
