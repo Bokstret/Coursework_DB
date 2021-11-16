f3b2b46efb5b055ff1a10d0302ea2c21f7cee18d
Bokstret <43569571+Bokstret@users.noreply.github.com>
2021-11-16 21:12:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from flask_session import Session
 
 
 app = Flask(__name__, static_url_path='')
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_TYPE'] = "filesystem"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vtzefyhigwptbe:45eec3aaa53b4719cf61196aad25f2b5ca825611542238856d9801fc557c34f1@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/d1pfe0st2g9c16'
 db = SQLAlchemy(app)
-Session(app)
+
 
 
 
@@ -26,11 +24,8 @@ def default():
     return render_template('index.html')
 
 
-@app.route('/login', methods=["POST", "GET"])
+@app.route('/login')
 def login():
-    if request.method == "POST":
-        session["name"] = 'ihorek'
-	return redirect("/")
     return render_template('login.html')
 
 
